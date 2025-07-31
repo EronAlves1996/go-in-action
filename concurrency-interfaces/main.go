5334be9383b83aea924ea838ef95124cf1d346e5
Eron Alves <eron.alves@rocketmail.com>
2025-07-31 16:46:10 -0300

    feat: make first version of counter without syncing

--- concurrency-interfaces/main.go
+++ concurrency-interfaces/main.go
@@ -3,9 +3,26 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"sync"
 )
 
 func main() {
+	var c Counter
+	var wg sync.WaitGroup
+	wg.Add(100)
+	for range 100 {
+		go func() {
+			defer wg.Done()
+			for range 1000 {
+				c.Increment()
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Printf("Value: %d\n", c.GetValue())
+}
+
+func polimorficLogger() {
 	c := ConsoleLogger{}
 	var b bytes.Buffer
 	f := FileLogger{
