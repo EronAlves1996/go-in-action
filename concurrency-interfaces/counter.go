b991f6c01695f6fcf513576f00adc56acb5c0e74
Eron Alves <eron.alves@rocketmail.com>
2025-07-31 16:49:29 -0300

    feat: make concurrent counter correct

--- concurrency-interfaces/counter.go
+++ concurrency-interfaces/counter.go
@@ -1,10 +1,15 @@
 package main
 
+import "sync"
+
 type Counter struct {
 	value int
+	mutex sync.Mutex
 }
 
 func (c *Counter) Increment() {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	c.value++
 }
 
