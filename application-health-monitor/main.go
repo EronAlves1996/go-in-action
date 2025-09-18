60b7b40b0356996c7cafbfa51c72400384a5193a
Eron Alves <eron.alves@rocketmail.com>
2025-09-17 23:44:03 -0300

    feat: define a healthstatus struct

--- application-health-monitor/main.go
+++ application-health-monitor/main.go
@@ -1,1 +1,19 @@
 package main
+
+import "time"
+
+type Status int
+
+const (
+	OK Status = iota
+	ERROR
+	WARNING
+)
+
+var StatusName = [...]string{"OK", "ERROR", "WARNING"}
+
+type HealthStatus struct {
+	Service   string
+	Status    Status
+	Timestamp time.Time
+}
